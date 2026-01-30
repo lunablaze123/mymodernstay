d1428ce0e093bf802a0ab6fa585e76f4a73f7f15
lunablaze123 <iamlunablaze@gmail.com>
2026-01-30 06:06:48 +0100

    Delete assets directory

--- assets/js/pages/admin.js
+++
@@ -1,2 +0,0 @@
-import { getJSON, applyTheme, mountNav, mountFooter, mountFadeIns } from "../app.js";
-(async ()=>{applyTheme(await getJSON("content/settings/theme.json")); await mountNav("admin.html"); await mountFooter(); mountFadeIns();})();
--- assets/media/video/README.txt
+++
@@ -1,1 +0,0 @@
-Place your hero.mp4 here (assets/media/video/hero.mp4). If missing, the site uses a poster image.
